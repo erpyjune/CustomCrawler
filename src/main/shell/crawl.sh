a9e394ee00cfeb974e778f53cc92dd9cdf087e7b
oj.bae <oj.bae@hanmail.net>
2015-05-11 14:20:35 +0900

    shell update

---
+++ src/main/shell/crawl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlMain
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor timon &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor coupang &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor wemef &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor g9 &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor gsdeal &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor lottethanksdeal &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor hotkill &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor happyvirusfirst &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor happyviruspost &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor happydeals &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor tongoutdoor &
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.CrawlOutdoor niio &
---
+++ src/main/shell/extract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp CrawlComponent-0.0.1-jar-with-dependencies.jar com.erpy.main.ExtractDataMain
